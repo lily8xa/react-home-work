a74d93993c2b808c8fc144ec6f3e6964e2dcf4ff
lily8xa <lily8xa@gmail.com>
2026-09-06 17:00:15 +0300

    завершила 3 домашнє(юзери і їх корзини)

--- src/component/cart/Cart.tsx
+++ src/component/cart/Cart.tsx
@@ -2,11 +2,11 @@ import type {FC} from "react";
 import type {CartType} from "../../models/CartType.ts";
 import './Cart.css'
 
-export const Cart:FC<CartType> = ({id,userId,products}) => {
+export const Cart:FC<CartType> = ({id,userId,products,totalProducts,total,discountedTotal}) => {
 
     return (
         <div className={'main-cart'}>
-            <h1>{id}. User-{userId}</h1>
+            <h1>{id}. User-{userId} Items in cart {totalProducts}, in the amount of-{total} USA, Discount-{discountedTotal} USA</h1>
             <ul>Products:{products.map((product,index)=>(<li  className={'one-cart'} key={index}>{product.title}.
                 {product.price} USD, <img src={product.thumbnail} alt='picture'/></li>))}</ul>
         </div>
